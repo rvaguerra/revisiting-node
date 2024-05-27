bd439a9f4eb4e951a39a2b3fedf0f1f94c302e70
Rodrigo Viégas <rvaguerra@gmail.com>
2024-05-27 16:34:24 -0300

    Login and POST handling

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    const { url } = req;
+    const { url, method } = req;
 
     if (url === '/about') {
         res
             .setHeader('Content-Type', 'application/json')
             .write(JSON.stringify({ page: 'About' }));
         return res.end();
+    }
+
+    if (url === '/login' && method === 'POST') {
+        const body = [];
+        req
+            .on('data', (chunk) => body.push(chunk))
+            .on('end', () => {
+                const { username, password } = JSON.parse(Buffer.concat(body).toString());
+                const result = username === 'username' && password === 'password';
+
+                res
+                    .setHeader('Content-Type', 'application/json')
+                    .write(JSON.stringify({ result }));
+                return res.end();
+            });
+        return;
     }
 
     res
