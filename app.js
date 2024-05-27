e46bf50f64c6faba0879eff13fa70c0d72cc4961
Rodrigo Viégas <rvaguerra@gmail.com>
2024-05-27 16:16:03 -0300

    Add about page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const { url } = req;
+
+    if (url === '/about') {
+        res
+            .setHeader('Content-Type', 'application/json')
+            .write(JSON.stringify({ page: 'About' }));
+        return res.end();
+    }
+
     res
         .setHeader('Content-Type', 'application/json')
         .write(JSON.stringify({ page: 'Home' }));
