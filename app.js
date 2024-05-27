fcefa699b4eecd08ca3ef7d6f35cef051f63bf77
Rodrigo Viégas <rvaguerra@gmail.com>
2024-05-27 17:37:01 -0300

    Refactoring routes

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
 const http = require('http');
+const routes = require('./routes.js');
 
-const server = http.createServer((req, res) => {
-    const { url, method } = req;
-
-    if (url === '/about') {
-        res
-            .setHeader('Content-Type', 'application/json')
-            .write(JSON.stringify({ page: 'About' }));
-        return res.end();
-    }
-
-    if (url === '/login' && method === 'POST') {
-        const body = [];
-        req
-            .on('data', (chunk) => body.push(chunk))
-            .on('end', () => {
-                const { username, password } = JSON.parse(Buffer.concat(body).toString());
-                const result = username === 'username' && password === 'password';
-
-                res
-                    .setHeader('Content-Type', 'application/json')
-                    .write(JSON.stringify({ result }));
-                return res.end();
-            });
-        return;
-    }
-
-    res
-        .setHeader('Content-Type', 'application/json')
-        .write(JSON.stringify({ page: 'Home' }));
-    return res.end();
-});
-
+const server = http.createServer(routes);
 server.listen(8080);
