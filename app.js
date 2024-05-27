f268edae25579f6d35171b1c2394165e3b6167de
Rodrigo Viégas <rvaguerra@gmail.com>
2024-05-27 16:12:27 -0300

    Add home!

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // 
+    res
+        .setHeader('Content-Type', 'application/json')
+        .write(JSON.stringify({ page: 'Home' }));
+    return res.end();
 });
 
 server.listen(8080);
