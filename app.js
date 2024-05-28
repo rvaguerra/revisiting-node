1ee40285c7be4b74c2e6be3ece480b5cff550d97
Rodrigo Viégas <rvaguerra@gmail.com>
2024-05-28 17:47:45 -0300

    Processing post requests

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (_, res) => {
     return res.send('Home');
 });
+
+app.get('/add-product', (req, res) => {
+    return res.send('<form method="POST" action="/add-product"><input type="text" name="product-name"><button type="submit">Add</button></form>');
+});
+
+app.post('/add-product', (req, res) => {
+    return res.send(req.body);
+})
 
 app.get('/about', (_, res) => {
     return res.send('About page!');
