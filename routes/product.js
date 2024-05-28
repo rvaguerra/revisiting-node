a9838b63610ae54495e94fe23723564d93bbd00e
Rodrigo Viégas <rvaguerra@gmail.com>
2024-05-28 18:04:29 -0300

    Product as a resource

--- routes/product.js
+++ routes/product.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res) => {
-    return res.send('<form method="POST" action="/add-product"><input type="text" name="product-name"><button type="submit">Add</button></form>');
+router.get('/', (req, res) => {
+    return res.send('<form method="POST" action="/product"><input type="text" name="product-name"><button type="submit">Add</button></form>');
 });
 
-router.post('/add-product', (req, res) => {
+router.post('', (req, res) => {
     return res.send(req.body);
 })
 
